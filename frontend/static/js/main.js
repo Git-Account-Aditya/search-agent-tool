9d9dbd3d95d381d83c09334c53ef7e4791d094a0
Coder-AdityaGithub <adityarawat717@gmail.com>
2025-09-28 17:57:56 +0530

    changes in base url

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -7,7 +7,12 @@ const currentResults = document.getElementById('currentResults');
 const historyList = document.getElementById('historyList');
 
 // API Base URL
-const API_BASE_URL = 'http://localhost:8000/api/v1';
+const API_BASE = window.location.hostname.includes("localhost")
+  ? "http://localhost:8000/api/v1"
+  : "https://search-agent-tool.onrender.com/api/v1";
+
+fetch(`${API_BASE}/search`);
+
 
 // Perform Search
 async function performSearch() {
